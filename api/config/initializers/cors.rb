e673a8e808232005831ca994413367d9bcf43658
ngoctoandhv <ngoctoandhv95@gmail.com>
2024-06-05 17:40:50 +0700

    add link FE handle cor

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -9,6 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins "example.com"
     origins "http://localhost:3001"
+    origins "https://profile-app-1.onrender.com/"
 
     resource "*",
       headers: :any,
