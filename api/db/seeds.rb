39d3a43039b439ef60b007d1ed011c398b209e94
ngoctoandhv <ngoctoandhv95@gmail.com>
2024-06-05 14:32:25 +0700

    Seed data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+name_categories = ["Name_1", "Name_2", "Name_3"]
+name_categories.each do |n|
+  Category.find_or_create_by!(name: n)
+end
+
+Category.ids.each do |id|
+  Post.find_or_create_by!(
+    title: Faker::Game.title,
+    content: Faker::Lorem.paragraph,
+    category_id: id
+  )
+end
