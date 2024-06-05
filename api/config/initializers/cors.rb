dcf300ebb44a9931c6617026977f13bcf7ea9076
ngoctoandhv <ngoctoandhv95@gmail.com>
2024-06-05 18:28:07 +0700

    add env api app

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,9 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins "example.com"
-    origins "http://localhost:3001"
-    origins "https://profile-app-1.onrender.com/"
+    origins ENV.fetch("REACT_APP_URL", "http://localhost:3001")
 
     resource "*",
       headers: :any,
