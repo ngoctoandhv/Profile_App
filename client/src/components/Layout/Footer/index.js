1c70c6668cfe2689259fe765765abee4d8e39fa7
ngoctoandhv <ngoctoandhv95@gmail.com>
2024-06-06 16:10:18 +0700

    add header and footer FE

---
+++ client/src/components/Layout/Footer/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <footer id="footer" className="footer">
+
+    <div className="container">
+      <div className="copyright text-center ">
+        <p>© <span>Copyright</span> <strong className="px-1 sitename">Dev</strong> <span>All Rights Reserved</span></p>
+      </div>
+      <div className="social-links d-flex justify-content-center">
+        <a href="/"><i className="bi bi-twitter-x"></i></a>
+        <a href="/"><i className="bi bi-facebook"></i></a>
+        <a href="/"><i className="bi bi-instagram"></i></a>
+        <a href="/"><i className="bi bi-linkedin"></i></a>
+      </div>
+      <div className="credits">
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+
+  </footer>
+  )
+}
+
+export default Footer;
